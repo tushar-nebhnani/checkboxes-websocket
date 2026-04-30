df8284c0630f98b92bbc30c195567c268455ec4e
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-04-30 14:30:30 +0530

    simulataneous working of checkboxes.

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import "dotenv/config";
 import express from "express";
 import { Server } from "socket.io";
 
+const CHECKBOX_SIZE = 1008;
+const state = {
+  checkboxes: new Array(CHECKBOX_SIZE).fill(false),
+};
+
 async function main() {
   const PORT = process.env.PORT ?? 8000;
 
@@ -20,6 +25,7 @@ async function main() {
     socket.on("client:checkbox:change", (data) => {
       console.log(`[Socket:${socket.id}:client:checkbox:change]`, data);
       io.emit("server:checkbox:change", data);
+      state.checkboxes[data.idx] = data.checked;
     });
   });
 
@@ -30,6 +36,10 @@ async function main() {
       healthy: true,
     }),
   );
+
+  app.get("/checkboxes", (req, res) => {
+    return res.json({ checkboxes: state.checkboxes });
+  });
 
   server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
