e6c0f2f71ee67a1a88ec27a33f28da476ec12058
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-08-02 15:59:11 +0530

    fix 2

--- backend/src/redis-connection.js
+++ backend/src/redis-connection.js
@@ -1,10 +1,12 @@
 import Redis from "ioredis";
 
 function createRedisConnection() {
-  return new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
-  });
+  return new Redis(
+    process.env.REDIS_URI || {
+      host: "localhost",
+      port: 6379,
+    },
+  );
 }
 
 export class RedisConnection {
