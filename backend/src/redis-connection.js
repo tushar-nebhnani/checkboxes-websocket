e389bf30c9ca34251ad2c3ff9cff79818fbaa24e
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2026-08-02 16:24:39 +0530

    fix 3

--- backend/src/redis-connection.js
+++ backend/src/redis-connection.js
@@ -2,7 +2,11 @@ import Redis from "ioredis";
 
 function createRedisConnection() {
   return new Redis(
-    process.env.REDIS_URI || {
+    process.env.REDIS_URI,
+    {
+      family: 4, // Forces Node to use IPv4 to fix Upstash DNS resolution timeouts
+      tls: {}, // Ensures the connection is encrypted, required by Upstash
+    } || {
       host: "localhost",
       port: 6379,
     },
